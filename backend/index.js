842d4f7c23fce9d471e6590a69e039f630754edd
Huzaifa-huzzy <lastpeople748@gmail.com>
2025-03-25 16:00:50 +0500

    new changes

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,16 @@ const signUpRoutes = require('./routes/SignUp');
 const productDataRoutes = require('./routes/ProductData');
 const cartRoutes = require('./routes/CartRoutes');
 
-// Load environment variables
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5175' })); // Uses CLIENT_URL
+app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5175' }));
 app.use(express.json());
 
-// Connect to database
 connectDB();
 
-// Routes
 app.get('/', (req, res) => {
   res.send('API running');
 });
@@ -28,7 +24,6 @@ app.use('/api', signUpRoutes);
 app.use('/api', productDataRoutes);
 app.use('/api/cart', cartRoutes);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
